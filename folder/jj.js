36feeab1ebfc750eb3daf34c62381c32ad855a49
Ell do <91648854+jamelle01@users.noreply.github.com>
2022-11-01 14:18:19 +0800

    pinis-retouch-later

---
+++ folder/jj.js
@@ -0,0 +1,6 @@
+fetch("./sampledata.json")
+    .then(() => {
+        console.log("fetch")
+    })
+
+    console.log("hello")
--- script.js
+++ script.js
@@ -1,71 +1,82 @@
-const video = document.getElementById('video')
+const video = document.getElementById('video');
+const vid = document.getElementsByClassName('vid')[0];
+const password = document.getElementById('password');
+password.style.display = "none"; // so that it won't show at first
 
-Promise.all([
+Promise.all([ // load all the models first before anything
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  // faceapi.nets.faceExpressionNet.loadFromUri('/modeels'),
   faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
 ]).then(c('video')).then(startVideo)
 
-function c(c){
+function c(c){ 
   console.log(c);
 }
-function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+function startVideo(){
+  // navigator.getUserMedia(
+  //   { video: {} },
+  //   stream => video.srcObject = stream,
+  //   err => console.error(err)
+  // )
+  navigator.mediaDevices.getUserMedia({
+    video: true
+  })
+  .then(stream => {
+    window.localStream = stream;
+    video.srcObject = stream;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 }
 
-
 video.addEventListener('play',async () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
   
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
-  c('canvas ready')  
-  const labeledFaceDescriptors = await loadLabeledImages()
-  c('image loaded')
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.60)
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  c('canvas ready');
+  const labeledFaceDescriptors = await loadLabeledImages();
+  c('image loaded');
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.60);
 
   const myInterval = setInterval(async () => {
-    c('detecting face')
-    const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors()
+    c('detecting face');
+    const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors();
     c('preparing the box')
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
     
-    canvas.getContext('2d').clearRect(0, 0, canvas.width,canvas.height)
-    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+    // canvas.getContext('2d').clearRect(0, 0, canvas.width,canvas.height)
+    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor)); // collect the result
     
-    let str = results.toString()
+    let str = results.toString(); //
     
     results.forEach(async (result, i) => {
-      const box = resizedDetections[i].detection.box
+      const box = resizedDetections[i].detection.box;
 
-      str = result.toString()
+      str = result.toString();
       var splitStr = str.split(" ");
       
-      
-      if (splitStr[0] !== "unknown"){
-        clearInterval(myInterval)
-        console.log("interval clear") // clear the time in interval resulting to stop scanning
-        const username = splitStr[0];
-        document.location.href = `https://www.google.com/${username}`;
+      if (splitStr[0] !== "unknown"){ //if not eq to unknown it proceed
+        clearInterval(myInterval);
+        console.log("interval clear"); // clear the time in interval resulting to stop scanning
+        console.log(splitStr[0]); // check
+        video.style.display = "none"; //hid the video  
+        vid.style.display = "none"; //hid the video  
+        localStream.getVideoTracks()[0].stop(); //
+        document.getElementById("username").innerText = (splitStr[0]);
+        password.style.display = "grid";
       }
-
-      const drawBox = new faceapi.draw.DrawBox(box, { label: splitStr[0] })
-      drawBox.draw(canvas)
     })
 
-  }, 100)
+  }, 100);
   return false;
 })
 
 function loadLabeledImages() {
-  const labels = ['Jeramelle', 'Maui', 'Paul', 'Justin']
+  const labels = ['jeramelle', 'justin'] // NEED TO BE SOMEWHERE AND FETCH
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
@@ -75,7 +86,6 @@ function loadLabeledImages() {
         descriptions.push(detections.descriptor)
         c("check images")
       }
-
       return new faceapi.LabeledFaceDescriptors(label, descriptions)
     })
   )
