352a53541af5022e0203d996e78c19050653f3ef
Ell do <91648854+jamelle01@users.noreply.github.com>
2022-11-30 02:25:47 +0800

    noting

--- script.js
+++ script.js
@@ -1,88 +1,103 @@
-const video = document.getElementById('video');
-const vid = document.getElementsByClassName('vid')[0];
-const password = document.getElementById('password');
+const video = document.getElementById("video");
+const vid = document.getElementsByClassName("vid")[0];
+const password = document.getElementById("password");
 password.style.display = "none"; // so that it won't show at first
 
-Promise.all([ // load all the models first before anything
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-]).then(c('video')).then(startVideo)
+Promise.all([
+  // load all the models first before anything
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+])
+  .then(c("video"))
+  .then(startVideo);
 
-function c(c){ 
+function c(c) {
   console.log(c);
 }
-function startVideo(){ // start video cam for scanning
-  navigator.mediaDevices.getUserMedia({
-    video: true
-  })
-  .then(stream => {
-    window.localStream = stream;
-    video.srcObject = stream;
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+function startVideo() {
+  // start video cam for scanning
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+    })
+    .then((stream) => {
+      window.localStream = stream;
+      video.srcObject = stream;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
-video.addEventListener('playing',async () => { // inig start sa video cam
+video.addEventListener("playing", async () => {
+  // inig start sa video cam
   const canvas = faceapi.createCanvasFromMedia(video);
   document.body.append(canvas);
-  
+
   const displaySize = { width: video.width, height: video.height };
   faceapi.matchDimensions(canvas, displaySize);
-  c('canvas ready');
+  c("canvas ready");
   const labeledFaceDescriptors = await loadLabeledImages();
-  c('image loaded');
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.60);
+  c("image loaded");
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
 
   const myInterval = setInterval(async () => {
-    c('detecting face');
-    const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors();
-    c('preparing the box')
+    c("detecting face");
+    const detections = await faceapi
+      .detectAllFaces(video)
+      .withFaceLandmarks()
+      .withFaceDescriptors();
+    c("preparing the box");
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    
+
     // canvas.getContext('2d').clearRect(0, 0, canvas.width,canvas.height)
-    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor)); // collect the result
-    
+    const results = resizedDetections.map((d) =>
+      faceMatcher.findBestMatch(d.descriptor)
+    ); // collect the result
+
     let str = results.toString(); //
-    
+
     results.forEach(async (result, i) => {
       const box = resizedDetections[i].detection.box;
 
       str = result.toString();
       var splitStr = str.split(" ");
-      
-      if (splitStr[0] !== "unknown"){ //if not eq to unknown it proceed
+
+      if (splitStr[0] !== "unknown") {
+        //if not eq to unknown it proceed
         clearInterval(myInterval);
         console.log("interval clear"); // clear the time in interval resulting to stop scanning
         console.log(splitStr[0]); // check
-        video.style.display = "none"; //hid the video  
-        vid.style.display = "none"; //hid the video  
+        video.style.display = "none"; //hid the video
         localStream.getVideoTracks()[0].stop(); //
         const result = splitStr[0].toUpperCase();
-        document.getElementById("username").innerText = (result);
+        document.getElementById("username").innerText = result;
         password.style.display = "grid";
       }
-    })
-
+    });
   }, 100);
   return false;
-})
+});
 
 function loadLabeledImages() {
-  const labels = ['jeramelle', 'jerico', 'eric'] // NEED TO BE SOMEWHERE AND FETCH
+  const labels = ["jeramelle", "jerico", "eric"]; // NEED TO BE SOMEWHERE AND FETCH
   return Promise.all(
-    labels.map(async label => {
-      const descriptions = []
+    labels.map(async (label) => {
+      const descriptions = [];
       for (let i = 1; i <= 1; i++) {
-        const img = await faceapi.fetchImage(`./labeled_images/${label}/${i}.jpg`)
-        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-        descriptions.push(detections.descriptor)
-        c("check images")
+        const img = await faceapi.fetchImage(
+          `./labeled_images/${label}/${i}.jpg`
+        );
+        const detections = await faceapi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        descriptions.push(detections.descriptor);
+        c("check images");
       }
-      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
     })
-  )
+  );
 }
--- try.js
+++ try.js
@@ -1,76 +1,70 @@
 let data = null;
 const error = null;
 
-fetch('./sampledata.json') // fetch the fake data naa sa sample
-    .then(res => {
-        if (!res.ok) { // error coming back from server
-            throw Error('could not fetch the data for that resource');
-        } 
-        return res.json()
-    })
-    .then(data => {
-        setData(data);
-    })
-    .then(()=>{
-        console.log(data)
-    })    
-    .catch(err => {
-        console.log(err);
-    })
+fetch("./sampledata.json") // fetch the fake data naa sa sample
+  .then((res) => {
+    if (!res.ok) {
+      // error coming back from server
+      throw Error("could not fetch the data for that resource");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    setData(data);
+  })
+  .then(() => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // document.getElementById("clickme").onclick = setTimeout(doFunction(), 1000);
 
-document.querySelector("#clickme")
-    .addEventListener('click', () => {
-        doFunction();
-    });
-document.getElementById("pass")
-    .addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-          event.preventDefault();
-          document.getElementById("clickme").click();
-        }
-      });
+document.querySelector("#clickme").addEventListener("click", () => {
+  doFunction();
+});
+document.getElementById("pass").addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("clickme").click();
+  }
+});
 
-
-
-function setData(d){
-    data = d;
-    console.log("data stored")
+function setData(d) {
+  data = d;
+  console.log("data stored");
 }
 
-function doFunction(){
-    console.log("function")
-    var t = false;
-    var user = document.getElementById("username").innerHTML;
-    var pass = document.getElementById("pass").value;
-    console.log(user)
-    console.log(pass)
-    for(let i=0; i<data.users.length; i++){
-        if(data.users[i].username.toLowerCase() == user.toLowerCase()){
-            console.log("forloop")
-            if(data.users[i].password == pass){
-                console.log("ok")
-                t = true;
-                window.location.href = "http://127.0.0.1:5500/index.html"; //redirect to login page
-                break;
-            }
-        }else{
-            t = false;
-        }
+function doFunction() {
+  console.log("function");
+  var t = false;
+  var user = document.getElementById("username").innerHTML;
+  var pass = document.getElementById("pass").value;
+  console.log(user);
+  console.log(pass);
+  for (let i = 0; i < data.users.length; i++) {
+    if (data.users[i].username.toLowerCase() == user.toLowerCase()) {
+      console.log("forloop");
+      if (data.users[i].password == pass) {
+        console.log("ok");
+        t = true;
+        window.location.href = "http://127.0.0.1:5500/index.html"; //redirect to login page
+        break;
+      }
+    } else {
+      t = false;
     }
-    console.log(t)
-    {!t && console.log("password not match")
+  }
+  console.log(t);
+  {
+    !t && console.log("password not match");
     document.getElementById("pass").style.border = "thick solid  #ff0000";
     document.getElementById("notif").innerText = "password not match";
-    }
-    setTimeout(() => {
-        
-    }, 5000);
+  }
+  setTimeout(() => {}, 5000);
 }
 
-const fdfd = ()=> {
-    console.log("wait")
-}
-
-
+const fdfd = () => {
+  console.log("wait");
+};
