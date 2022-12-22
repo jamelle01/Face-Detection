ba739d7840621b573fa7d9bb69722d74773188be
Ell do <91648854+jamelle01@users.noreply.github.com>
2022-12-23 05:36:30 +0800

    try

--- try.js
+++ try.js
@@ -1,4 +1,12 @@
 let user;
+var clientMac = getQueryStringKey("clientMac");
+var apMac = getQueryStringKey("apMac");
+// var gatewayMac = getQueryStringKey("gatewayMac") || undefined;
+var ssidName = getQueryStringKey("ssidName") || undefined;
+var radioId = !!getQueryStringKey("radioId")
+  ? Number(getQueryStringKey("radioId"))
+  : undefined;
+
 async function fetchWorkouts() {
   const response = await fetch(
     "https://face-recognition-backend.adaptable.app/"
@@ -34,7 +42,7 @@ document.getElementById("pass").addEventListener("keypress", function (event) {
 
 // function setData(d) {
 
-function doFunction() {
+async function doFunction() {
   console.log("function");
   var t = false;
   var userd = document.getElementById("username").innerHTML;
@@ -57,6 +65,13 @@ function doFunction() {
     document.getElementById("notif").innerText = "";
   }
   console.log(t);
+  if (t) {
+    // await fetchController();
+    console.log(clientMac);
+    console.log(apMac);
+    console.log(ssidName);
+    console.log(radioId);
+  }
   {
     !t && console.log("password not match");
     if (!t) {
@@ -65,4 +80,69 @@ function doFunction() {
     }
   }
   setTimeout(() => {}, 5000);
+}
+
+const fetchController = () => {
+  const authInfo = {
+    clientMac: clientMac,
+    apMac: apMac,
+    ssidName: ssidName,
+    radioId: radioId,
+    time: milliseconds,
+    authType: 4,
+  };
+  console.log("f");
+};
+
+function getQueryStringKey(key) {
+  return getQueryStringAsObject()[key];
+}
+function getQueryStringAsObject() {
+  var b,
+    cv,
+    e,
+    k,
+    ma,
+    sk,
+    v,
+    r = {},
+    d = function (v) {
+      return decodeURIComponent(v);
+    }, //# d(ecode) the v(alue)
+    q = window.location.search.substring(1), //# suggested: q = decodeURIComponent(window.location.search.substring(1)),
+    s = /([^&;=]+)=?([^&;]*)/g; //# original regex that does not allow for ; as a delimiter:   /([^&=]+)=?([^&]*)/g
+  ma = function (v) {
+    if (typeof v != "object") {
+      cv = v;
+      v = {};
+      v.length = 0;
+      if (cv) {
+        Array.prototype.push.call(v, cv);
+      }
+    }
+    return v;
+  };
+  while ((e = s.exec(q))) {
+    b = e[1].indexOf("[");
+    v = d(e[2]);
+    if (b < 0) {
+      k = d(e[1]);
+      if (r[k]) {
+        r[k] = ma(r[k]);
+        Array.prototype.push.call(r[k], v);
+      } else {
+        r[k] = v;
+      }
+    } else {
+      k = d(e[1].slice(0, b));
+      sk = d(e[1].slice(b + 1, e[1].indexOf("]", b)));
+      r[k] = ma(r[k]);
+      if (sk) {
+        r[k][sk] = v;
+      } else {
+        Array.prototype.push.call(r[k], v);
+      }
+    }
+  }
+  return r;
 }
