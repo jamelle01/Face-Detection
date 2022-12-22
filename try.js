810556e135095d7663861655f092bf78449b0295
Ell do <91648854+jamelle01@users.noreply.github.com>
2022-12-23 07:30:59 +0800

    TRY

--- try.js
+++ try.js
@@ -73,7 +73,7 @@ async function doFunction() {
     console.log(ssidName);
     console.log(radioId);
     console.log(redirectUrl);
-    window.location.href = `${redirectUrl}`; //redirect to login page
+    // window.location.href = `${redirectUrl}`; //redirect to login page
   }
   {
     !t && console.log("password not match");
@@ -85,7 +85,7 @@ async function doFunction() {
   setTimeout(() => {}, 5000);
 }
 
-const fetchController = () => {
+async function authorize() {
   const authInfo = {
     clientMac: clientMac,
     apMac: apMac,
@@ -93,12 +93,57 @@ const fetchController = () => {
     radioId: radioId,
     authType: 4,
   };
-  console.log("f");
-};
+
+  // get CSRF token
+  const csrfToken = getCSRFToken();
+
+  // define headers
+  const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    // "Csrf-Token": csrfToken
+  };
+
+  // define fetch options
+  const options = {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(authInfo),
+    // credentials: "include",
+  };
+
+  const CONTROLLER = "192.168.100.80";
+  const PORT = "8043";
+  const CONTROLLER_ID = "93575f5c1d2898597019560a983a0794";
+
+  // make HTTP request using fetch function
+  const res = await fetch(
+    `https://${CONTROLLER}:${PORT}/${CONTROLLER_ID}/api/v2/hotspot/login`,
+    options
+  );
+
+  // parse response as JSON
+  const resObj = await res.json();
+
+  // check if authorization was successful
+  if (resObj.errorCode === 0) {
+    // authorized successfully
+    window.location.href = `${redirectUrl}`; //redirect to login page
+  }
+
+  // function to get CSRF token
+  // function getCSRFToken() {
+  //   // use filesystem module to read token from file
+  //   const fs = require("fs");
+  //   const token = fs.readFileSync(TOKEN_FILE_PATH, "utf8");
+  //   return token;
+  // }
+}
 
 function getQueryStringKey(key) {
   return getQueryStringAsObject()[key];
 }
+
 function getQueryStringAsObject() {
   var b,
     cv,
