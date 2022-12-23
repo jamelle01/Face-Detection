a9d7865620eddc8cb2fba9bd6b65a337f2d93792
Ell do <91648854+jamelle01@users.noreply.github.com>
2022-12-23 08:53:15 +0800

    try

--- try.js
+++ try.js
@@ -41,8 +41,6 @@ document.getElementById("pass").addEventListener("keypress", function (event) {
   }
 });
 
-// function setData(d) {
-
 async function doFunction() {
   console.log("function");
   var t = false;
@@ -74,7 +72,8 @@ async function doFunction() {
     console.log(radioId);
     console.log(redirectUrl);
     // window.location.href = `${redirectUrl}`; //redirect to login page/
-    authorize();
+    // authorize();
+    login();
   }
   {
     !t && console.log("password not match");
@@ -86,61 +85,92 @@ async function doFunction() {
   setTimeout(() => {}, 5000);
 }
 
-async function authorize() {
-  const authInfo = {
-    clientMac: clientMac,
-    apMac: apMac,
-    ssidName: ssidName,
-    radioId: radioId,
-    authType: 4,
+const CONTROLLER = "192.168.100.80";
+const PORT = "8043";
+const CONTROLLER_ID = "93575f5c1d2898597019560a983a0794";
+async function login() {
+  // define login info
+  const loginInfo = {
+    name: "tplink",
+    password: "tplink",
   };
-
-  // get CSRF token
-  // const csrfToken = getCSRFToken();
 
   // define headers
   const headers = {
     "Content-Type": "application/json",
     Accept: "application/json",
-    // "Csrf-Token": csrfToken
   };
 
   // define fetch options
   const options = {
     method: "POST",
     headers: headers,
-    body: JSON.stringify(authInfo),
-    // credentials: "include",
+    body: JSON.stringify(loginInfo),
+    credentials: "include",
   };
-
-  const CONTROLLER = "192.168.100.80";
-  const PORT = "8043";
-  const CONTROLLER_ID = "93575f5c1d2898597019560a983a0794";
 
   // make HTTP request using fetch function
   const res = await fetch(
-    // `https://${CONTROLLER}:${PORT}/${CONTROLLER_ID}/api/v2/hotspot/login`,
-    `https://${CONTROLLER}:${PORT}/${CONTROLLER_ID}/api/v2/hotspot/extPortal/auth`,
+    `https://${CONTROLLER}:${PORT}/${CONTROLLER_ID}/api/v2/hotspot/login`,
     options
   );
-
-  // parse response as JSON
-  const resObj = await res.json();
-
-  // check if authorization was successful
-  if (resObj.errorCode === 0) {
-    // authorized successfully
-    window.location.href = `${redirectUrl}`; //redirect to login page
-  }
-
-  // function to get CSRF token
-  // function getCSRFToken() {
-  //   // use filesystem module to read token from file
-  //   const fs = require("fs");
-  //   const token = fs.readFileSync(TOKEN_FILE_PATH, "utf8");
-  //   return token;
-  // }
 }
+
+// async function authorize() {
+//   const authInfo = {
+//     clientMac: clientMac,
+//     apMac: apMac,
+//     ssidName: ssidName,
+//     radioId: radioId,
+//     authType: 4,
+//   };
+
+//   // get CSRF token
+//   // const csrfToken = getCSRFToken();
+
+//   // define headers
+//   const headers = {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//     // "Csrf-Token": csrfToken
+//   };
+
+//   // define fetch options
+//   const options = {
+//     method: "POST",
+//     headers: headers,
+//     body: JSON.stringify(authInfo),
+//     // credentials: "include",
+//   };
+
+//   const CONTROLLER = "192.168.100.80";
+//   const PORT = "8043";
+//   const CONTROLLER_ID = "93575f5c1d2898597019560a983a0794";
+
+//   // make HTTP request using fetch function
+//   const res = await fetch(
+//     // `https://${CONTROLLER}:${PORT}/${CONTROLLER_ID}/api/v2/hotspot/login`,
+//     `https://${CONTROLLER}:${PORT}/${CONTROLLER_ID}/api/v2/hotspot/extPortal/auth`,
+//     options
+//   );
+
+//   // parse response as JSON
+//   const resObj = await res.json();
+
+//   // check if authorization was successful
+//   if (resObj.errorCode === 0) {
+//     // authorized successfully
+//     window.location.href = `${redirectUrl}`; //redirect to login page
+//   }
+
+//   // function to get CSRF token
+//   // function getCSRFToken() {
+//   //   // use filesystem module to read token from file
+//   //   const fs = require("fs");
+//   //   const token = fs.readFileSync(TOKEN_FILE_PATH, "utf8");
+//   //   return token;
+//   // }
+// }
 
 function getQueryStringKey(key) {
   return getQueryStringAsObject()[key];
