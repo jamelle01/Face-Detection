efc01dc5ba34653d3823d55bf69f55b03f95c352
Ell do <91648854+jamelle01@users.noreply.github.com>
2022-12-23 15:08:40 +0800

    trt

--- try.js
+++ try.js
@@ -112,8 +112,8 @@ async function login() {
 
   // make HTTP request using fetch function
   const res = await fetch(
-    // `https://${CONTROLLER}:${PORT}/93575f5c1d2898597019560a983a0794/api/v2/hotspot/login`,
-    `https://omada.tplinkcloud.com/omada/5.7.4/index.html?token=964d114725b24bc2bebf22514ed08f4d&deviceId=7B0695175E87B1B626A4DA68330D1DECF7BF89&omadacId=93575f5c1d2898597019560a983a0794`,
+    `https://${CONTROLLER}:${PORT}/93575f5c1d2898597019560a983a0794/api/v2/hotspot/login`,
+    // `https://omada.tplinkcloud.com/omada/5.7.4/index.html?token=964d114725b24bc2bebf22514ed08f4d&deviceId=7B0695175E87B1B626A4DA68330D1DECF7BF89&omadacId=93575f5c1d2898597019560a983a0794`,
     options
   );
 }
