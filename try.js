46dbe266054be6bda5934ea98ed4227195863a0c
Ell do <91648854+jamelle01@users.noreply.github.com>
2022-12-23 09:59:16 +0800

    try

--- try.js
+++ try.js
@@ -113,7 +113,7 @@ async function login() {
   // make HTTP request using fetch function
   try {
     const res = await fetch(
-      `https://${CONTROLLER}:${PORT}/${CONTROLLER_ID}/login`,
+      `https://${CONTROLLER}:${PORT}/${CONTROLLER_ID}`,
       options
     );
   } catch (error) {
